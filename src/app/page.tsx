7fdc777271a203bfda2733150a8eca0f4399d443
gstupart <gstupart@uw.edu>
2024-06-01 18:46:31 -0700

    Fixed pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormLabel from '@mui/material/FormLabel';
+import Pagination from '@mui/material/Pagination';
 
 function Home() {
     // State to manage search input, search results, selected criteria, error, and pagination
@@ -25,10 +26,9 @@ function Home() {
         inputProps: { 'aria-label': 'Checkbox for search queries' }
     };
 
-    // Function to handle search with pagination
     const handleSearch = async (page = 1, limit = 15) => {
         try {
-            let url = `http://localhost:4000/books/search?page=${page}&limit=${limit}`;
+            let url = `http://localhost:4000/books/search?page=${page}&offset=1000000`;
             let criteria = [];
             
             // Constructing the URL based on selected criteria
@@ -44,14 +44,55 @@ function Home() {
             
             // Joining criteria with '&'
             url += '&' + criteria.join('&');
+            console.log(url);
+            
+            // Fetch data based on the constructed URL
+            const response = await fetch(url);
+            if (response.ok) {
+                const data = await response.json();
+                const tPages = Math.ceil(data.books.length / limit);
+                setTotalPages(tPages);
+                console.log(tPages);
+                handlePageChangeSearch(page, limit);
+            } else {
+                const errorMessage = await response.text();
+                setError("Invalid");
+                setSearchResults([]);
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setError('Error fetching data. Please try again later.');
+            setSearchResults([]);
+        }
+    };
+
+    // Function to handle search with pagination
+    const handlePageChangeSearch = async (page = 1, limit = 15) => {
+        try {
+            let url = `http://localhost:4000/books/search?page=${page}&offset=${limit}`;
+            let criteria = [];
+            
+            // Constructing the URL based on selected criteria
+            if (selectedCriteria.author) criteria.push(`author=${searchText}`);
+            if (selectedCriteria.isbn) criteria.push(`isbn=${searchText}`);
+            if (selectedCriteria.title) criteria.push(`title=${searchText}`);
+            if (selectedCriteria.range) criteria.push(`${searchText}`);
+            // If no criteria selected, return
+            if (criteria.length === 0) {
+                setError('Please select one of the options to search.');
+                return;
+            }
+            
+            // Joining criteria with '&'
+            url += '&' + criteria.join('&');
+            console.log(url);
             
             // Fetch data based on the constructed URL
             const response = await fetch(url);
             if (response.ok) {
                 const data = await response.json();
                 setSearchResults(data.books);
-
-                setTotalPages(data.totalPages);
+                console.log(data.books);
                 setCurrentPage(page);
                 setError("");
 
@@ -74,6 +115,14 @@ function Home() {
         }
     };
 
+    // Debug Function, can be deleted
+    useEffect(() => {
+        console.log('Updated searchResults:', searchResults);
+        console.log('Current page', currentPage)
+        console.log('Total pages', totalPages)
+        
+    }, [searchResults]);
+
     // Function to handle checkbox selection
     const handleCheckboxChange = (criteria) => {
         setSelectedCriteria({
@@ -83,8 +132,9 @@ function Home() {
     };
 
     // Function to handle pagination
-    const handlePagination = (page) => {
-        handleSearch(page);
+    const handlePagination = (event: React.ChangeEvent<unknown>, page: number) => {
+        setCurrentPage(page);
+        handlePageChangeSearch(page);
     };
 
     return (
@@ -127,22 +177,6 @@ function Home() {
                         </FormGroup>
                         <FormHelperText sx={{color: 'red'}}>Check 1 or more:</FormHelperText>
                     </FormControl>
-                    {/* <label>
-                        ISBN
-                        <input
-                            type="checkbox"
-                            checked={selectedCriteria.isbn}
-                            onChange={() => handleCheckboxChange('isbn')}
-                        />
-                    </label>
-                    <label>
-                        Title
-                        <input
-                            type="checkbox"
-                            checked={selectedCriteria.title}
-                            onChange={() => handleCheckboxChange('title')}
-                        />
-                    </label> */}
                     <label>
                         Range (Type offset=?&min=?)
                         <input
@@ -159,7 +193,7 @@ function Home() {
                         <h2>Search Results:</h2>
                         <ul>
                             {searchResults
-                                .slice((currentPage - 1) * 15, currentPage * 15)
+                                .slice(0, 15)
                                 .map((book, index) => (
                                     <li key={book.id || index}>
                                     
@@ -174,20 +208,14 @@ function Home() {
                                 ))}
                         </ul>
                     </div>
-                )}
-                {searchResults.length > 5 && (
-                <div>
-                    <button onClick={() => handlePagination(currentPage - 1)} disabled={currentPage === 1}>
-                        Previous 
-                    </button>
-                    <span>Page {currentPage} of {Math.ceil(searchResults.length /15)}</span>
-                    <button onClick={() => handlePagination(currentPage + 1)} disabled={currentPage === Math.ceil(searchResults.length / 15)}>
-                        Next 
-                    </button>
-                </div>
-                )}
+            )}
+            {searchResults.length > 5 && (
+            <div>
+                <Pagination count={totalPages} color="primary" onChange={handlePagination} />
+            </div>
+            )}
         
-                {error && <p>Error: {error}</p>}
+            {error && <p>Error: {error}</p>}
         </div>
     );
 }
